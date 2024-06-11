9353a52fb20fd9211435ce71cf64b9afa60a725e
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-06-11 23:20:18 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+import os, uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "comet.main:app",
+        host=os.getenv("HOST", "127.0.0.1"),
+        port=int(os.getenv("PORT", "8000")),
+        # workers=int(os.getenv("WORKERS", 2*(os.cpu_count() or 1))),
+        ssl_keyfile="./key.pem", 
+        ssl_certfile="./cert.pem"
+    )
