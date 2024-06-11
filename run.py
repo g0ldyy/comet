7caeabddd085e18e75681105c2b6e1bb62cf5886
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-06-11 23:21:34 +0200

    add info

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         "comet.main:app",
         host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "8000")),
-        # workers=int(os.getenv("WORKERS", 2*(os.cpu_count() or 1))),
+        # workers=int(os.getenv("WORKERS", 2*(os.cpu_count() or 1))), # Disabled for development
         ssl_keyfile="./key.pem", 
         ssl_certfile="./cert.pem"
     )
