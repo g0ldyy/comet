520fbf9fff2e4fcde507e71d749ec40b7b332e4d
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-06-15 17:02:53 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
-import os, uvicorn
+import os, uvicorn, dotenv
+
+dotenv.load_dotenv()
 
 if __name__ == "__main__":
     uvicorn.run(
         "comet.main:app",
         host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "8000")),
-        workers=int(os.getenv("WORKERS", 2*(os.cpu_count() or 1))), # Disabled for development
-        # ssl_keyfile="./key.pem", # For development
-        # ssl_certfile="./cert.pem" # For development
+        workers=int(os.getenv("FASTAPI_WORKERS", 2*(os.cpu_count() or 1))),
+        ssl_keyfile="./key.pem", # For development
+        ssl_certfile="./cert.pem" # For development
     )
