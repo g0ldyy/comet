7769a5c41d33cfee3cafa33e84bbdd7a97beac39
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-06-15 17:09:55 +0200

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "8000")),
         workers=int(os.getenv("FASTAPI_WORKERS", 2*(os.cpu_count() or 1))),
-        ssl_keyfile="./key.pem", # For development
-        ssl_certfile="./cert.pem" # For development
+        # ssl_keyfile="./key.pem", # For development
+        # ssl_certfile="./cert.pem" # For development
     )
