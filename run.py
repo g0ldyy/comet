02c6d8503c5d4455e2e61b22f942cd5e71e250a8
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-06-25 11:59:31 +0200

    fix env

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ dotenv.load_dotenv()
 if __name__ == "__main__":
     uvicorn.run(
         "comet.main:app",
-        host=os.getenv("HOST", "127.0.0.1"),
-        port=int(os.getenv("PORT", "8000")),
+        host=os.getenv("FASTAPI_HOST", "127.0.0.1"),
+        port=int(os.getenv("FASTAPI_PORT", "8000")),
         workers=int(os.getenv("FASTAPI_WORKERS", 2*(os.cpu_count() or 1))),
         # ssl_keyfile="./key.pem", # For development
         # ssl_certfile="./cert.pem" # For development
