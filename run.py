e1c0ef87d6399935361dfb73861ff6c597a96922
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-06-29 00:14:55 +0200

    very needed

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         "comet.main:app",
         host=os.getenv("FASTAPI_HOST", "127.0.0.1"),
         port=int(os.getenv("FASTAPI_PORT", "8000")),
-        workers=int(os.getenv("FASTAPI_WORKERS", 2*(os.cpu_count() or 1))),
+        workers=int(os.getenv("FASTAPI_WORKERS", 2*(os.cpu_count() or 1)))
     )
