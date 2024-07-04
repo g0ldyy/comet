268664b698784cdfad1bf631d80cb6264a3e2fc2
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-07-05 01:37:29 +0200

    typo

--- comet/api/stream.py
+++ comet/api/stream.py
@@ -300,7 +300,7 @@ async def playback(request: Request, b64config: str, hash: str, index: str):
                         while True:
                             chunk = await response.content.read(
                                 settings.PROXY_DEBRID_STREAM_BYTES_PER_CHUNK
-                            )  # 10 MB chunks
+                            )
                             if not chunk:
                                 break
                             yield chunk
