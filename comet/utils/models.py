05fe06294b0471a2cd8e7cfb3c3b96a9863e1f6a
Goldy <153996346+g0ldyy@users.noreply.github.com>
2024-11-17 00:46:43 +0100

    feat: ability to disable max connection #3

--- comet/utils/models.py
+++ comet/utils/models.py
@@ -37,7 +37,7 @@ class AppSettings(BaseSettings):
     CUSTOM_HEADER_HTML: Optional[str] = None
     PROXY_DEBRID_STREAM: Optional[bool] = False
     PROXY_DEBRID_STREAM_PASSWORD: Optional[str] = None
-    PROXY_DEBRID_STREAM_MAX_CONNECTIONS: Optional[int] = 100
+    PROXY_DEBRID_STREAM_MAX_CONNECTIONS: Optional[int] = -1
     PROXY_DEBRID_STREAM_DEBRID_DEFAULT_SERVICE: Optional[str] = "realdebrid"
     PROXY_DEBRID_STREAM_DEBRID_DEFAULT_APIKEY: Optional[str] = None
     TITLE_MATCH_CHECK: Optional[bool] = True
