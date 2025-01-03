a5ff4d4bd2dc1e203109e40f54ff1ddd45084c1a
Goldy <153996346+g0ldyy@users.noreply.github.com>
2025-01-03 15:25:38 +0100

    Update core.py

--- comet/api/core.py
+++ comet/api/core.py
@@ -1,7 +1,5 @@
 import PTT
 import RTN
-import random
-import string
 
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
@@ -63,7 +61,7 @@ async def manifest(b64config: str = None):
     debrid_extension = get_debrid_extension(config["debridService"])
 
     return {
-        "id": f"{settings.ADDON_ID}.{''.join(random.choice(string.ascii_letters) for _ in range(4))}",
+        "id": settings.ADDON_ID,
         "name": f"{settings.ADDON_NAME}{(' | ' + debrid_extension) if debrid_extension is not None else ''}",
         "description": "Stremio's fastest torrent/debrid search add-on.",
         "version": "1.0.0",
