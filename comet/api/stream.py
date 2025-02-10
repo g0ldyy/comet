c98c3d450e10a49885cf028aadb3668a55cb7c1f
Goldy <153996346+g0ldyy@users.noreply.github.com>
2025-02-10 21:45:48 +0100

    fix: only get file index and size from torrent_file_indexes if in direct torrent mode

--- comet/api/stream.py
+++ comet/api/stream.py
@@ -182,27 +182,28 @@ async def stream(
                 torrent_data["parsed"] if torrent_data["cached"] else torrent.data
             )
 
-            cached_index = await database.fetch_one(
-                """
-                SELECT file_index, file_size 
-                FROM torrent_file_indexes 
-                WHERE info_hash = :info_hash
-                AND ((cast(:season as INTEGER) IS NULL AND season IS NULL) OR season = cast(:season as INTEGER))
-                AND ((cast(:episode as INTEGER) IS NULL AND episode IS NULL) OR episode = cast(:episode as INTEGER))
-                AND timestamp + :cache_ttl >= :current_time
-                """,
-                {
-                    "info_hash": info_hash,
-                    "season": metadata["season"],
-                    "episode": metadata["episode"],
-                    "cache_ttl": settings.CACHE_TTL,
-                    "current_time": time.time(),
-                },
-            )
+            if debrid_service == "torrent":
+                cached_index = await database.fetch_one(
+                    """
+                    SELECT file_index, file_size 
+                    FROM torrent_file_indexes 
+                    WHERE info_hash = :info_hash
+                    AND ((cast(:season as INTEGER) IS NULL AND season IS NULL) OR season = cast(:season as INTEGER))
+                    AND ((cast(:episode as INTEGER) IS NULL AND episode IS NULL) OR episode = cast(:episode as INTEGER))
+                    AND timestamp + :cache_ttl >= :current_time
+                    """,
+                    {
+                        "info_hash": info_hash,
+                        "season": metadata["season"],
+                        "episode": metadata["episode"],
+                        "cache_ttl": settings.CACHE_TTL,
+                        "current_time": time.time(),
+                    },
+                )
 
-            if cached_index:
-                torrent_data["fileIndex"] = cached_index["file_index"]
-                torrent_data["size"] = cached_index["file_size"]
+                if cached_index:
+                    torrent_data["fileIndex"] = cached_index["file_index"]
+                    torrent_data["size"] = cached_index["file_size"]
 
             debrid_emoji = (
                 "🧲"
