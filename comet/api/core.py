4df905e72aa2892ff65248d439a57d7db1a7683f
g0ldyy <153996346+g0ldyy@users.noreply.github.com>
2025-08-23 15:23:49 +0200

    refactor: remove assertion for orjson installation in CustomORJSONResponse

--- comet/api/core.py
+++ comet/api/core.py
@@ -84,7 +84,6 @@ class CustomORJSONResponse(Response):
     media_type = "application/json"
 
     def render(self, content):
-        assert orjson is not None, "orjson must be installed"
         return orjson.dumps(content, option=orjson.OPT_INDENT_2)
 
 
