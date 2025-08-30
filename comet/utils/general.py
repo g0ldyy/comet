17f27966f884b6910d9d3a0366ee9ae6c1359385
g0ldyy <153996346+g0ldyy@users.noreply.github.com>
2025-08-30 18:12:27 +0200

    fix: handle commas in size string for size_to_bytes function

--- comet/utils/general.py
+++ comet/utils/general.py
@@ -89,6 +89,8 @@ def size_to_bytes(size_str: str):
     sizes = ["b", "kb", "mb", "gb", "tb"]
 
     value, unit = size_str.split()
+
+    value = value.replace(",", "")
     value = float(value)
     unit = unit.lower()
 
